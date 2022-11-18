a531839b7859b4d40e27521ca7662e137290180f
Tom Chapman <tomchapman128@gmail.com>
2022-11-18 23:43:23 +0000

    Added word collater to turn raw data into json data

---
+++ scrape4me/word_collater.py
@@ -0,0 +1,36 @@
+from os import path
+import json
+
+RAW_WORD_ENCOUNTERS_FILE = 'word_encounters.txt'
+COLLATED_WORD_ENCOUNTERS_FILE = 'word_data.json'
+
+raw_word_encounters_path = path.join(path.dirname(path.abspath(__file__)), RAW_WORD_ENCOUNTERS_FILE)
+collated_word_encounters_path = path.join(path.dirname(path.abspath(__file__)), COLLATED_WORD_ENCOUNTERS_FILE)
+
+word_encounters = {}
+total_word_encounters = 0
+
+with open(raw_word_encounters_path, 'r', encoding='utf-8') as file:
+    for line in file:
+        word = line.strip()
+        if word not in word_encounters:
+            word_encounters[word] = 0
+        
+        word_encounters[word] += 1
+        total_word_encounters += 1
+
+print(f'Loaded {len(word_encounters)} unique words out of {total_word_encounters} total word encounters')
+
+# sort alphabetically
+word_encounters = {k: v for k, v in sorted(word_encounters.items(), key=lambda item: item[0])}
+
+json_data = {
+    'words': [
+        {'word': word, 'frequency': encounters} for word, encounters in word_encounters.items()
+    ]
+}
+
+with open(collated_word_encounters_path, 'w', encoding='utf-8') as file:
+    json.dump(json_data, file, indent=4)
+
+print(f'Saved word encounters to {COLLATED_WORD_ENCOUNTERS_FILE}')
