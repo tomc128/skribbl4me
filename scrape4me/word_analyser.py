8fc92116d7561d6705e5f3c1e92a6e82ecc46a56
Tom Chapman <tomchapman128@gmail.com>
2022-11-18 23:50:25 +0000

    Added word analyser to plot most common words

---
+++ scrape4me/word_analyser.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import json
+from os import path
+
+COLLATED_WORD_ENCOUNTERS_FILE = 'word_data.json'
+collated_word_encounters_path = path.join(path.dirname(path.abspath(__file__)), COLLATED_WORD_ENCOUNTERS_FILE)
+
+with open(collated_word_encounters_path, 'r', encoding='utf-8') as file:
+    json_data = json.load(file)
+
+words = json_data['words']
+
+# plot a bar chart of word against frequency
+
+top_words = sorted(words, key=lambda word: word['frequency'], reverse=True)[:20]
+top_words = [(word['word'], word['frequency']) for word in top_words]
+
+# plot
+plt.bar(*zip(*top_words))
+plt.xticks(rotation=45)
+plt.show()
